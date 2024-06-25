15d9b6522493eb5aef1c02065ceaf5a824f4eae4
Sofia Bargues <barguessofia@gmail.com>
2024-06-25 13:17:10 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div className="relative overflow-hidden w-[181px] h-screen    m-auto">
+      <img
+        src="./src/assets/bg-top.svg"
+        className="left-[50%] top-[-2%]
+         
+       absolute -z-10"
+        width={180}
+      ></img>
+      <div
+        className="bg-[#f7f7ff]  w-full h-full
+       absolute -z-20"
+      ></div>
+      <div>Our Pricing</div>
+      <div>Annually Monthly</div>
+      Basic $199.99 500 CB Storage 2 Users Allowed Send up to 3 CB LEARN MORE
+    </div>
+  );
+}
+
+export default App;
