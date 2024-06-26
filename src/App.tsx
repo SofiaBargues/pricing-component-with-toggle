0a1c03adc64cbced5ce7a98ddcc2bedfc81986a1
Sofia Bargues <barguessofia@gmail.com>
2024-06-26 03:11:33 +0100

    tamano de pricing

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,72 @@ function App() {
         className="bg-[#f7f7ff]  w-full h-full
        absolute -z-20"
       ></div>
-      <div className="font-bold text-3xl text-[#6f7390] flex justify-center m-10 ">
-        Our Pricing
+      <div className="m-9">
+        <div className="font-bold text-3xl text-[#6f7390] flex justify-center m-10 ">
+          Our Pricing
+        </div>
+        <div className="flex justify-center gap-3 items-center flex-row text-[#aeaeba] relative font-bold text-md">
+          <div className="">Annually</div>
+          <div className="bg-white left-[133px] h-5 rounded-full w-5 absolute"></div>
+          <div className="bg-[#7a7ee1] w-12 h-7 rounded-full"></div>
+          <div className="">Monthly</div>
+        </div>
       </div>
-      <div className="flex justify-center gap-3 items-center flex-row text-[#aeaeba] relative font-bold text-md">
-        <div className="">Annually</div>
-        <div className="bg-white left-[169px] h-5 rounded-full w-5 absolute"></div>
-        <div className="bg-[#7a7ee1] w-12 h-7 rounded-full"></div>
-        <div className="">Monthly</div>
+      <div className="flex flex-col gap-[32px] my-6">
+        <div className="rounded-lg w-[323px] h-[455px] text-sm  font-bold text-[#4c4f60] bg-white m-auto p-6 flex items-center justify-between flex-col ">
+          <div className="text-lg text-[#666882]">Basic</div>
+          <div className="text-7xl flex items-center">
+            <span className="text-4xl">$</span>199.99
+          </div>
+          <div className="  w-full text-md text-[#6b6b83] flex flex-col gap-6 text-center ">
+            <hr></hr>
+            <div>500 GB Storage</div>
+            <hr></hr>
+            <div>2 Users Allowed</div>
+            <hr></hr>
+            <div>Send up to 3 CB</div>
+            <hr></hr>
+          </div>
+          <div className="bg-[#7276e1] text-white font-semibold text-center  p-3 rounded-lg w-full">
+            LEARN MORE
+          </div>
+        </div>
+        <div className="rounded-lg w-[323px] h-[455px] text-sm  font-bold text-[#4c4f60] bg-white m-auto p-6 flex items-center justify-between flex-col">
+          <div className="text-lg text-[#666882]">Basic</div>
+          <div className="text-7xl flex items-center">
+            <span className="text-4xl">$</span>199.99
+          </div>
+          <div className="  w-full text-md text-[#6b6b83] flex flex-col gap-6 text-center ">
+            <hr></hr>
+            <div>500 GB Storage</div>
+            <hr></hr>
+            <div>2 Users Allowed</div>
+            <hr></hr>
+            <div>Send up to 3 CB</div>
+            <hr></hr>
+          </div>
+          <div className="bg-[#7276e1] text-white font-semibold text-center  p-3 rounded-lg w-full">
+            LEARN MORE
+          </div>
+        </div>
+        <div className="rounded-lg w-[323px] h-[455px] text-sm  font-bold text-[#4c4f60] bg-white m-auto p-6 flex items-center justify-between flex-col">
+          <div className="text-lg text-[#666882]">Basic</div>
+          <div className="text-7xl flex items-center">
+            <span className="text-4xl">$</span>199.99
+          </div>
+          <div className="  w-full text-md text-[#6b6b83] flex flex-col gap-6 text-center ">
+            <hr></hr>
+            <div>500 GB Storage</div>
+            <hr></hr>
+            <div>2 Users Allowed</div>
+            <hr></hr>
+            <div>Send up to 3 CB</div>
+            <hr></hr>
+          </div>
+          <div className="bg-[#7276e1] text-white font-semibold text-center  p-3 rounded-lg w-full">
+            LEARN MORE
+          </div>
+        </div>
       </div>
       <div className="flex flex-col gap-[32px] my-6">
         <div className="w-[323px] h-[455px] text-sm  font-bold text-[#4c4f60] bg-white m-auto p-6 flex items-center justify-between flex-col ">
