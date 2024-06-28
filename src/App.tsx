8fba7dbd85a8d690253550f21c7fbabf19935d4d
Sofia Bargues <barguessofia@gmail.com>
2024-06-28 11:59:38 +0100

    toggle

--- src/App.tsx
+++ src/App.tsx
@@ -41,10 +41,10 @@ function Card({
   return (
     <div
       className={
-        " w-[323px] rounded-md h-[455px] text-sm m-auto font-bold p-6 flex items-center justify-between flex-col " +
+        " w-[323px] rounded-md h-[455px] text-sm m-auto font-bold md:m-0 p-12 flex items-center justify-between flex-col " +
         (name != "Professional"
           ? "   text-[#4c4f60] bg-white "
-          : "  text-white bg-[#9b9fee]")
+          : "  text-white bg-[#9b9fee] md:h-[490px] ")
       }
     >
       <div
@@ -89,30 +89,40 @@ function Card({
 
 function App() {
   return (
-    <div className="font-montserrat flex flex-col relative overflow-hidden w-[374px] h-full  justify-center  m-auto">
+    <div className="font-montserrat   flex flex-col md:justify-start relative overflow-hidden w-[374px] md:w-screen md:h-screen  h-full  justify-center  m-auto">
       <img
         src="./src/assets/bg-top.svg"
-        className="left-[50%] top-[-1%]
-         
-       absolute -z-10"
+        className="left-[50%] top-[-1%] absolute -z-10 block md:hidden"
         width={550}
       ></img>
+      <img
+        src="./src/assets/bg-top.svg"
+        className="left-[50%] top-[-1%] absolute -z-10 md:block  hidden"
+        width={250}
+      ></img>
+      <img
+        src="./src/assets/bg-bottom.svg"
+        className="left-[0%] bottom-[0%] absolute -z-10 md:block hidden"
+        width={290}
+      ></img>
       <div
-        className="bg-[#f7f7ff]  w-full h-full
+        className="bg-[#f7f7ff] w-full h-full
        absolute -z-20"
       ></div>
       <div className="m-9">
-        <div className="font-bold text-3xl text-[#6f7390] flex justify-center m-10 ">
+        <div className="font-bold text-3xl text-[#6f7390] flex justify-center m-10 md:m-0 md:p-10 ">
           Our Pricing
         </div>
-        <div className="flex justify-center gap-3 items-center flex-row text-[#aeaeba] relative font-bold text-md">
+        <div className="flex justify-center gap-3 items-center flex-row text-[#aeaeba] font-bold text-md">
           <div className="">Annually</div>
-          <div className="bg-white left-[130px] rounded-full w-6 h-6  absolute"></div>
-          <div className="bg-[#7a7ee1] w-14 h-8 rounded-full"></div>
+
+          <div className="bg-[#7a7ee1] w-14 h-8 rounded-full relative ">
+            <div className="bg-white left-[4px] top-1 rounded-full w-6 h-6 absolute"></div>
+          </div>
           <div className="">Monthly</div>
         </div>
       </div>
-      <div className="flex flex-col gap-[32px] my-6">
+      <div className="flex flex-col md:items-center gap-[32px] my-6 md:flex-row md:gap-0 justify-center">
         {plans.map((plan) => (
           <Card
             name={plan.name}
